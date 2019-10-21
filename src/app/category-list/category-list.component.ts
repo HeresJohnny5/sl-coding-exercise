e9015740e296cbcdbe9c48fecd8f2a2aef6b311a
HeresJohnny5 <ericksonjohn01@hotmail.com>
2019-10-20 20:01:22 -0400

    Use of more Angular approach rather append HTML content dynamically.

--- src/app/category-list/category-list.component.ts
+++ src/app/category-list/category-list.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import * as $ from 'jquery';
-
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
@@ -13,48 +11,5 @@ export class CategoryListComponent implements OnInit {
 
   ngOnInit() { }
 
-  onClickAddCategory() {
-    const tbRowCount = $("tbody tr").length + 1;
-    const draggableIcon = "../../assets/images/icon/icon-draggable/Group 11070.svg";
-
-    $('tbody').append(`
-      <tr id="tb-row-${tbRowCount}">
-        <td class="category category-col-1">
-          <span>
-            <img src="../../assets/images/icon/icon-draggable/Group 11070.svg" class="remove-category" alt="Remove Category">
-          </span>
-        </td>
-        <td class="category category-col-2">
-          <input type="text" name="category" placeholder="category">
-        </td>
-        <td class="category category-col-3">
-          <span>1</span>
-        </td>
-        <td class="category category-col-4">
-          <span>
-            <img src="../../assets/images/icon/icon-remove/Group 15571.svg" class="remove-category" alt="Remove Category">
-          </span>
-        </td>
-      </tr>
-    `);
-  }
+  private tableFieldArray: Array<any> = [];
 }
-
-// <tr id="tb-row-1">
-//   <td class="category category-col-1">
-//     <span>
-//       <img src="../../assets/images/icon/icon-draggable/Group 11070.svg" class="draggable-icon" alt="Draggable Category Icon">
-//     </span>
-//   </td>
-//   <td class="category category-col-2">
-//     <span>Category 1</span>
-//   </td>
-//   <td class="category category-col-3">
-//     <span>1</span>
-//   </td>
-//   <td class="category category-col-4">
-//     <span>
-//       <img src="../../assets/images/icon/icon-remove/Group 15571.svg" class="remove-category" alt="Remove Category">
-//     </span>
-//   </td>
-// </tr>
